4f1a0d8b1782404803b74c325df9f1edfd25a993
reality <tinmachin3@gmail.com>
2016-02-27 18:59:03 +0000

    add upload thing and more instructions

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-Just change the req.api_key line and the req.external_path variables in server/app.js to whatever you like, then run bin/www.
 
-Then you can upload things to the server with e.g. curl --form "fileupload=@test.txt" --form api_key=fdjiaofjai849309809ahionanlnkjzbahhfhwa http://iliketurtles.com/upload
+1. Change the req.api_key line and the req.external_path variables in server/app.js to whatever you like, then run bin/www.
+2. Change the api_key and url to the previously configured options in the alsuti file
+3. Add alias alsuti='~/whereverthisrepois/alsuti' to your .bashrc
+4. Then you can run alsuti whateveryouwanttoupload.gif
+
+Note: if you use fish, you can add an alias instead by doing:
+
+```
+function alsuti
+  ~/whereverthisrepois/alsuti $argv[1]
+end
+funcsave alsuti
+```
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,7 @@ var express = require('express'),
 
 /* GET home page. */
 router.post('/upload', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'application/text');
 
   if(req.body.api_key == req.api_key) {
     fs.readFile(req.files.fileupload.path, function(err, data) {
@@ -14,17 +14,11 @@ router.post('/upload', function(req, res) {
           newPath = __dirname + '/../public/';
 
       fs.writeFile(newPath + newName, data, function(err) {
-        res.send(JSON.stringify({
-          'err': null,
-          'msg': req.external_path + newName 
-        })); 
+        res.send(req.external_path + newName); 
       });
     });
   } else {
-    res.send(JSON.stringify({
-      'err': true,
-      'msg': 'API key incorrect'
-    })); 
+    res.send('Error: Incorrect API key');
   }
 });
 
