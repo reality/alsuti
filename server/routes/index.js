025879b7b269948781a6b41c30ae30c0500ef13f
reality <tinmachin3@gmail.com>
2016-02-27 18:16:32 +0000

    initial commit

---
+++ server/routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+    fs = require('fs'),
+    shortid = require('shortid'),
+    _ = require('underscore')._,
+    router = express.Router();
+
+/* GET home page. */
+router.post('/upload', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  if(req.body.api_key == req.api_key) {
+    fs.readFile(req.files.fileupload.path, function(err, data) {
+      var newName = shortid.generate() + '.' + _.last(req.files.fileupload.originalname.split('.'))
+                newPath = __dirname + '/../public/';
+          console.log(newPath);
+      fs.writeFile(newPath + newName, data, function(err) {
+        res.send(JSON.stringify({
+          'err': null,
+          'msg': req.external_path + newName 
+        })); 
+      });
+    });
+  } else {
+    res.send(JSON.stringify({
+      'err': true,
+      'msg': 'API key incorrect'
+    })); 
+  }
+});
+
+module.exports = router;
