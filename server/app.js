e298c010825a5e8b09658b194b543ab425c84a3b
reality <tinmachin3@gmail.com>
2016-02-28 12:25:33 +0000

    use environment variables

--- server/app.js
+++ server/app.js
@@ -5,9 +5,19 @@ var logger = require('morgan');
 var multer = require('multer');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var _ = require('underscore')._;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+if(!_.has(process.env, 'ALSUTI_API_KEY')) {
+  console.log('You must set the ALSUTI_API_KEY environment variable');
+  process.exit(1);
+}
+if(!_.has(process.env, 'ALSUTI_ENDPOINT') {
+  console.log('You must set the ALSUTI_ENDPOINT environment variable');
+  process.exit(1);
+}
 
 var app = express();
 
@@ -16,7 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -27,8 +37,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add some stuff to the req
 app.use(function(req, res, next) {
-  req.api_key = '';
-  req.external_path = 'http://reality.rehab/al/';
+  req.api_key = process.env.ALSUTI_API_KEY;
+  req.external_path = process.env.ALSUTI_ENDPOINT;
   next();
 });
 
@@ -36,9 +46,9 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -46,23 +56,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
